35f25de0b1fcf7e6107c238d240601a77a2c6aab
Deven Umrania <umraniadeven10@gmail.com>
2021-03-20 21:29:23 +0530

    Update: Dark-Light Background-Color for sections

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const checkbox = document.getElementById("checkbox");
 const label = document.getElementById("label");
 const header = document.getElementById("header");
+const projectSection = document.getElementById("PROJECTS");
+const blogSection = document.getElementById("BLOGS");
+const aboutSection = document.getElementById("ABOUT");
+const lineBreak = document.querySelectorAll("sectionBorderBottom");
+const profileImg = document.getElementById("profileImg");
+
 checkbox.addEventListener("change", () => {
     header.classList.toggle("headerDark");
     label.classList.toggle("labelDark");
+    projectSection.classList.toggle("contentDark");
+    blogSection.classList.toggle("contentDark");
+    aboutSection.classList.toggle("contentDark");
+    lineBreak.classList.toggle("sectionBorderBottomDark");
+    profileImg.classList.toggle("profileImgDark");
 })
