0e45ca7b9371e89130c5bee8848d338e8ea905a5
Deven Umrania <umraniadeven10@gmail.com>
2021-03-22 00:12:45 +0530

    Feature: Added Dark Theme (Home Page only)

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const blogSection = document.getElementById("BLOGS");
 const aboutSection = document.getElementById("ABOUT");
 const lineBreak = document.querySelectorAll("sectionBorderBottom");
 const profileImg = document.getElementById("profileImg");
+const aboutTitle = document.getElementById("about-title");
+const aboutDescription = document.getElementById("about-description");
+const viewAllProjects = document.getElementById("viewAllProjects");
+const viewAllBlogs = document.getElementById("viewAllBlogs");
+const blog = document.getElementById("blog");
+const project = document.getElementById("project");
+
 
 checkbox.addEventListener("change", () => {
     header.classList.toggle("headerDark");
@@ -13,6 +20,12 @@ checkbox.addEventListener("change", () => {
     projectSection.classList.toggle("contentDark");
     blogSection.classList.toggle("contentDark");
     aboutSection.classList.toggle("contentDark");
-    lineBreak.classList.toggle("sectionBorderBottomDark");
+    //lineBreak.classList.toggle("sectionBorderBottomDark");
     profileImg.classList.toggle("profileImgDark");
+    aboutTitle.classList.toggle("aboutTitleDark");
+    aboutDescription.classList.toggle("aboutDescriptionDark");
+    viewAllProjects.classList.toggle("viewAllProjectsDark");
+    viewAllBlogs.classList.toggle("viewAllBlogsDark");
+    blog.classList.toggle("blogDark");
+    project.classList.toggle("projectDark");
 })
